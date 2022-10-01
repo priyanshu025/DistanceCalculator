87db360820dd5e59bb0cb8db0e41b04eb6903d74
priyanshu <priyanshukumar025@gmail.com>
2022-10-02 01:53:36 +0530

    first commit

---
+++ Main.js
@@ -0,0 +1,42 @@
+﻿var myLatLng = { lat: 38.3460, lng: -0.4907 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+};
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+directionsDisplay.setMap(map);
+function calcRoute() {
+    var request = {
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            const output = document.querySelector('#output');
+            output.innerHTML = "<div ><b>Driving distance  :</b> " + result.routes[0].legs[0].distance.text + ".<br /><b>Duration :</b> " + result.routes[0].legs[0].duration.text + ".</div>";
+            directionsDisplay.setDirections(result);
+        } else {
+            directionsDisplay.setDirections({ routes: [] });
+            map.setCenter(myLatLng);
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+        }
+    });
+
+}
+var options = {
+    types: ['(cities)']
+}
+
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
